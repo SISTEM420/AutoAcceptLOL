c17ced86087abb1177fae743f6d3421d9630a1ac
unknown <arturrojorge@gmail.com>
2023-12-14 12:18:26 -0300

    first commit

--- autoaceptarlol.py
+++ autoaceptarlol.py
@@ -1,24 +1,32 @@
+#Auto-Accept LOL
+#V. 1.1.0
+#Arturo Jorge
+
 import tkinter as tk
 import pyautogui as pya
 from ctypes import windll
 from threading import Thread
 from pynput.mouse import Listener
 
+#region Set
 def set_color(x, y):
     global pos
     pos = (x, y)
 
-def set_click(x, y):
+def set_button(x, y):
     global posclick
     posclick = (x, y)
 
+#endregion
+
+#region mouse input
 def on_click_color(x, y, button, pressed):
     if pressed:
         set_color(x, y)
         return False
-def on_click_click(x, y, button, pressed):
+def on_click_button(x, y, button, pressed):
     if pressed:
-        set_click(x, y)
+        set_button(x, y)
         return False
     
 def leer_color():
@@ -28,11 +36,39 @@ def leer_color():
 
 def leer_click():
     # Crear un listener para el evento de clic del ratón
-    with Listener(on_click=on_click_click) as listener:
+    with Listener(on_click=on_click_button) as listener:
         listener.join()  # Inicia el listener
 
+#endregion
 
-def mostrar_esperando(stop):
+#region auto-accept match
+def accept_match(stop, waiting_window):
+    hdc = windll.user32.GetDC(0)
+    found_color = 1591641
+    
+    while stop[0]:
+        pixel_color = windll.gdi32.GetPixel(hdc, pos[0], pos[1])
+        
+        if pixel_color == found_color:
+            for i in range(0, 30):
+                pya.click(posclick[0], posclick[1])
+                i += 1
+                
+            stop[0] = False
+        else:
+            pass
+    
+    waiting_window.destroy()
+
+def exec_and_return():
+    stop = [True]
+    waiting_window = show_waiting(stop)
+    Thread(target=accept_match, args=(stop, waiting_window)).start()
+
+#endregion
+
+#region gui
+def show_waiting(stop):
     def detener():
         stop[0] = False
         waiting_window.destroy()
@@ -40,7 +76,7 @@ def mostrar_esperando(stop):
     waiting_window = tk.Toplevel(root)
     waiting_window.title("Esperando...")
     waiting_window.geometry("400x300")
-    waiting_window.config(bg="#2B2B2B")  # Fondo oscuro
+    waiting_window.config(bg="#2B2B2B")  # Dark background
 
     waiting_label = tk.Label(waiting_window, text="Esperando...", font=("Arial", 12), bg="#2B2B2B", fg="#0080FF")  # Texto celeste sobre fondo oscuro
     waiting_label.pack(padx=50, pady=50)
@@ -54,43 +90,24 @@ def mostrar_esperando(stop):
 
     return waiting_window
 
-def ejecutar_codigo(stop, waiting_window):
-    hdc = windll.user32.GetDC(0)
-    color_apagado = 1591641
-    
-    while stop[0]:
-        color_actual = windll.gdi32.GetPixel(hdc, pos[0], pos[1])
-        
-        if color_actual == color_apagado:
-            for i in range(0, 30):
-                pya.click(posclick[0], posclick[1])
-                i += 1
-                
-            stop[0] = False
-        else:
-            pass
-    
-    waiting_window.destroy()
 
-def ejecutar_y_volver():
-    stop = [True]
-    waiting_window = mostrar_esperando(stop)
-    Thread(target=ejecutar_codigo, args=(stop, waiting_window)).start()
 
-# Crear la interfaz gráfica principal
+# Creates the main interface
 root = tk.Tk()
 root.title("Auto-aceptar partidas")
 root.geometry("400x400")
-root.config(bg="#2B2B2B")  # Fondo oscuro
-
-# Botón para ejecutar el código
-main_label = tk.Label(root, text="Auto-aceptar partidas de lol", font=("Arial", 12), bg="#2B2B2B", fg="white")  # Texto blanco sobre fondo oscuro
+root.config(bg="#2B2B2B")  # Dark background
+main_label = tk.Label(root, text="Auto-aceptar partidas de lol", font=("Arial", 12), bg="#2B2B2B", fg="white")  # Text
 main_label.pack(padx=50, pady=50)
-definir_btn = tk.Button(root, text="Definir color", command=leer_color, bg="#444444", fg="white")  # Botón oscuro con texto blanco
-definir_btn.pack()
-definir_click_btn = tk.Button(root, text="Definir aceptar", command=leer_click, bg="#444444", fg="white")  # Botón oscuro con texto blanco
-definir_click_btn.pack()
-ejecutar_btn = tk.Button(root, text="Esperar partida", command=ejecutar_y_volver, bg="#444444", fg="white")  # Botón oscuro con texto blanco
-ejecutar_btn.pack()
+
+# Buttons
+
+define_color_btn = tk.Button(root, text="Definir color", command=leer_color, bg="#444444", fg="white")  
+define_color_btn.pack()
+define_place_to_click_btn = tk.Button(root, text="Definir aceptar", command=leer_click, bg="#444444", fg="white")  
+define_place_to_click_btn.pack()
+wait_btn = tk.Button(root, text="Esperar partida", command=exec_and_return, bg="#444444", fg="white")  # Start Button
+
+#endregion
 
 root.mainloop()
